56046d48d574707b9dd01402c547dee06c348452
JosephSmiley <smileyjo@umich.edu>
2024-04-04 17:50:24 -0400

    created main menu and login UI as well as database creation

--- app/src/main/java/com/example/brainardy/MainActivity.kt
+++ app/src/main/java/com/example/brainardy/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.brainardy
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -13,31 +15,32 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.example.brainardy.ui.theme.BrainardyTheme
 
 class MainActivity : ComponentActivity() {
+
+    private lateinit var dbHelper: MyDBHelper
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            BrainardyTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
-                }
-            }
+        setContentView(R.layout.activity_main)
+
+        dbHelper = MyDBHelper(this)
+        val db = dbHelper.readableDatabase
+
+        var btnPlay = findViewById<Button>(R.id.buttonPlay)
+        btnPlay.setOnClickListener {
+            val intent = Intent(this, PlayActivity::class.java)
+            startActivity(intent)
         }
-    }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name!",
-            modifier = modifier
-    )
-}
+        var btnEdit = findViewById<Button>(R.id.buttonEditQuestions)
+        btnEdit.setOnClickListener {
+            val intent = Intent(this, EditActivity::class.java)
+            startActivity(intent)
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    BrainardyTheme {
-        Greeting("Android")
+        }
+
+        var btnLogin = findViewById<Button>(R.id.buttonSignUp)
+        btnLogin.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
